6f3c7a8f90d92bfc6b4fa4ed42b45d7b34af6577
developer <developer@amicos-MacBook-Pro.local>
2025-09-22 17:21:29 -0700

    Added initial modules and files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # adventure-game-llm
 Full stack application built using FastAPI &amp; Python and JavaScript &amp; React. It works with database, contains AI integration, backend and frontend communication, Rest API, and various best practices.
+
+https://www.youtube.com/watch?v=_1P0Uqk50Ps
+
+    How to initialize this project
+1) adventure-game-llm % cd backend 
+   backend % ls
+   backend % pip3 install pydantic-settings
+   backend % uv init .
+   backend % uv add fastapi\[all\] langchain langchain
+-openai python-dotenv sqlalchemy uvicorn psycopg2-binary
---
+++ backend/core/config.py
@@ -0,0 +1,25 @@
+from typing import List
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    API_PREFIX: str = "/api"
+    DEBUG: bool = True
+
+    ALLOWED_ORIGINS: str = ""
+    OPEN_AI_KEY: str = ""
+
+    @field_validator("ALLOWED_ORIGINS")
+    def parse_allowed_origins(cls, value: str) -> List[str]:
+        return value.split(",") if value else []
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensetive = True
+
+
+settings = Settings()
